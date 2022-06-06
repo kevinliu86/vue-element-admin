e888163b003f529ecb0074663500641be7b4c704
kevinliu <yanchaoliu76@gmail.com>
2022-06-06 16:23:46 +1000

    change page head

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  title: 'Vue Element Admin',
+  title: "Kevin Liu's Demo",
 
   /**
    * @type {boolean} true | false
--- src/utils/get-page-title.js
+++ src/utils/get-page-title.js
@@ -1,6 +1,6 @@
 import defaultSettings from '@/settings'
 
-const title = defaultSettings.title || 'Vue Element Admin'
+const title = defaultSettings.title || "Kevin Liu's Demo"
 
 export default function getPageTitle(pageTitle) {
   if (pageTitle) {
